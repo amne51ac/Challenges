b0f61bab5cff3bf307385219e35e5af66649c79c
amne51ac <th3amne51ac@gmail.com>
2017-04-26 20:38:04 -0500

    Completed Challenge 2
    Still needs to handle lists, also started challenge 3

---
+++ Misc/2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 26 20:27:51 2017
+
+@author: amne51ac
+"""
+
+a = input("Please enter a number")
+
+for i in range(1,a):
+    a = a * i
+
+print a
---
+++ Misc/3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 26 20:34:53 2017
+
+@author: amne51ac
+"""
+
